16854973790eba5d36fad0823ccdbdd35373128d
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-05-11 18:45:23 +0000

    Renamed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="django_schedule",
+    name="django-schedule",
     version="0.1.0",
     packages=find_packages(),
     description="Django integration with schedule module",
