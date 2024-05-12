27509dde227f00a656213cf09cf52ec4a47f726d
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-05-12 06:46:17 +0000

    Fixed: command not available

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-easy-schedule",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     description="Django integration with schedule module",
     author="Waqqas Jabbar",
